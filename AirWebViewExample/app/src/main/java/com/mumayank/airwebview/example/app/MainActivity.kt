66092326a85a9349a212990c7296e5d53a035c0a
Mayank Mohan Upadhyay <mumayank@gmail.com>
2024-09-27 19:03:45 +0530

    init commit
    Signed-off-by: Mayank Upadhyay <mayupa@mcb.mu>

---
+++ AirWebViewExample/app/src/main/java/com/mumayank/airwebview/example/app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mumayank.airwebview.example.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mumayank.airwebview.example.app.ui.theme.AirWebViewExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AirWebViewExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AirWebViewExampleTheme {
+        Greeting("Android")
+    }
+}
