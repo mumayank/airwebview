9679c757eaa276d18c23c1b7ab787a4016ca7c0e
Mayank Upadhyay <mayupa@mcb.mu>
2024-09-28 12:04:10 +0530

    update to java 17
    Signed-off-by: Mayank Upadhyay <mayupa@mcb.mu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-group = "com.github.mumayank"
-version = "0.1.2" // This should match your Git tag
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
